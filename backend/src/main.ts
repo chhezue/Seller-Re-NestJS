24839c5d6c95517919218d1f8f13cbba27bcf227
손채연 <cyson@pmirnc.com>
2025-07-23 14:00:29 +0900

    build: 초기 설정

---
+++ backend/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // ConfigService 인스턴스 가져오기
+  const configService = app.get(ConfigService);
+
+  // 환경 변수 확인용 로그
+  console.log('✅ Supabase URL:', configService.get('SUPABASE_URL'));
+  console.log('✅ Supabase Key:', configService.get('SUPABASE_ANON_KEY'));
+
+  await app.listen(3000);
+}
+bootstrap();
