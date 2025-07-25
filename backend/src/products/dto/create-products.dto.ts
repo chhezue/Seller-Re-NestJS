67593a909d56a87d6448024a918ae93c4e9260a9
손채연 <cyson@pmirnc.com>
2025-07-25 17:24:07 +0900

    feat(product): products entity 설정 (2)

---
+++ backend/src/products/dto/create-products.dto.ts
@@ -0,0 +1,64 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+import {
+  PRODUCT_CONDITION,
+  PRODUCT_STATUS,
+  TRADE_TYPE,
+} from '../const/products.const';
+
+export class CreateProductsDto {
+  @ApiProperty({ description: '제품명' })
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty({ description: '제품 상세 설명' })
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @ApiProperty({ description: '카테고리 ID' })
+  @IsUUID()
+  @IsNotEmpty()
+  categoryId: string;
+
+  @ApiProperty({ description: '가격' })
+  @IsNumber()
+  @IsNotEmpty()
+  price: number;
+
+  // @ApiProperty({ description: '판매자 ID' })
+  // @IsUUID()
+  // sellerId: string;
+  //
+  // @ApiProperty({ description: '거래 지역 ID' })
+  // @IsUUID()
+  // regionId: string;
+
+  @ApiProperty({ description: '판매 상태', enum: PRODUCT_STATUS })
+  @IsEnum(PRODUCT_STATUS)
+  @IsNotEmpty()
+  status: PRODUCT_STATUS;
+
+  @ApiProperty({ description: '거래 형식', enum: TRADE_TYPE })
+  @IsEnum(TRADE_TYPE)
+  @IsNotEmpty()
+  tradeType: TRADE_TYPE;
+
+  @ApiProperty({ description: '제품 상태', enum: PRODUCT_CONDITION })
+  @IsEnum(PRODUCT_CONDITION)
+  @IsNotEmpty()
+  condition: PRODUCT_CONDITION;
+
+  @ApiProperty({ description: '가격 제안 가능 여부' })
+  @IsBoolean()
+  @IsNotEmpty()
+  isNegotiable: boolean;
+}
--- backend/src/products/entity/products.entity.ts
+++ backend/src/products/entity/products.entity.ts
@@ -1,6 +1,5 @@
 import { BaseModel } from '../../common/entity/base.entity';
 import { Column, Entity } from 'typeorm';
-import { IsBoolean, IsEnum, IsNumber, IsString, IsUUID } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import {
   PRODUCT_CONDITION,
@@ -12,61 +11,49 @@ import {
 export class ProductsModel extends BaseModel {
   @ApiProperty({ description: '제품명' })
   @Column()
-  @IsString()
   name: string;
 
   @ApiProperty({ description: '제품 상세 설명' })
   @Column()
-  @IsString()
   description: string;
 
   @ApiProperty({ description: '카테고리 ID' })
   @Column()
-  @IsUUID()
   categoryId: string;
 
   @ApiProperty({ description: '가격' })
   @Column()
-  @IsNumber()
   price: number;
 
   @ApiProperty({ description: '판매자 ID' })
   @Column()
-  @IsUUID()
   sellerId: string;
 
   @ApiProperty({ description: '거래 지역 ID' })
   @Column()
-  @IsUUID()
   regionId: string;
 
   @ApiProperty({ description: '판매 상태', enum: PRODUCT_STATUS })
   @Column({ default: PRODUCT_STATUS.ON_SALE })
-  @IsEnum(PRODUCT_STATUS)
-  status: ProductStatus;
+  status: PRODUCT_STATUS;
 
   @ApiProperty({ description: '거래 형식', enum: TRADE_TYPE })
   @Column({ default: TRADE_TYPE.SELL })
-  @IsEnum(TRADE_TYPE)
-  tradeType: TradeType;
+  tradeType: TRADE_TYPE;
 
   @ApiProperty({ description: '제품 상태', enum: PRODUCT_CONDITION })
   @Column({ default: PRODUCT_CONDITION.USED })
-  @IsEnum(PRODUCT_CONDITION)
-  condition: ProductCondition;
+  condition: PRODUCT_CONDITION;
 
   @ApiProperty({ description: '가격 제안 가능 여부' })
   @Column()
-  @IsBoolean()
   isNegotiable: boolean;
 
   @ApiProperty({ description: '조회수' })
   @Column({ default: 0 })
-  @IsNumber()
   viewCount: number;
 
   @ApiProperty({ description: '즐겨찾기 수' })
   @Column({ default: 0 })
-  @IsNumber()
   favoriteCount: number;
 }
