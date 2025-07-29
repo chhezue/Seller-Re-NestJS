96e979226037270ee8ab7e89f5863e881b6d463f
kimgyejeong <pmwkdo@gmail.com>
2025-07-29 14:40:28 +0900

    feat: 🔑 Implement email login with Basic Authentication

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Headers } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async postLoginEmail(@Headers('authorization') rawToken: string) {
+    const token = this.authService.extractTokenFromHeader(rawToken, false);
+
+    const credentials = this.authService.decodedBasicToken(token);
+
+    return await this.authService.loginWithEmail();
+  }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  extractTokenFromHeader(headerRawToken: string, isBearer: boolean) {
+    const splitToken = headerRawToken.split(' ');
+    const prefix = isBearer ? 'Bearer' : 'Basic';
+    if (splitToken.length !== 2 || splitToken[0] !== prefix) {
+      throw new UnauthorizedException('Invalid token format');
+    }
+
+    return splitToken[1];
+  }
+
+  decodedBasicToken(base64String: string) {
+    const decoded = Buffer.from(base64String, 'base64').toString('utf8');
+    const [email, password] = decoded.split(':');
+
+    if (!email || !password) {
+      throw new UnauthorizedException('Invalid Base Token(email or password)');
+    }
+
+    return { email, password };
+  }
+
+  async loginWithEmail() {}
+}
