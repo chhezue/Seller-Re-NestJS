6a6a657891289be91b5954b6b28066d7cb6590f2
kimgyejeong <pmwkdo@gmail.com>
2025-07-30 16:05:38 +0900

    feat: 🔑 Implement JWT generation for user authentication

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { EventEmitter2 } from '@nestjs/event-emitter';
 import * as bcrypt from 'bcrypt';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
@@ -12,6 +13,7 @@ export class AuthService {
     private readonly userService: UsersService,
     private readonly eventEmitter: EventEmitter2,
     private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
   ) {}
 
   extractTokenFromHeader(headerRawToken: string, isBearer: boolean) {
@@ -89,8 +91,24 @@ export class AuthService {
   //TODO. jwt token
   loginUser(user: Pick<UsersModel, 'email' | 'id'>) {
     return {
-      accessToken: `accessToken ${user.email} ${user.id}`,
-      refreshToken: `refreshToken ${user.email} ${user.id}`,
+      accessToken: this.signToken(user, false),
+      refreshToken: this.signToken(user, true),
     };
+  }
+
+  signToken(user: Pick<UsersModel, 'email' | 'id'>, isRefreshToken: boolean) {
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      type: isRefreshToken ? 'refresh' : 'access',
+    };
+
+    return this.jwtService.sign(payload, {
+      secret:
+        this.configService.get<string>(JWT_SECRET_KEY) +
+        this.configService.get<string>(CAT),
+      // refresh: 1d, access: 1h
+      expiresIn: isRefreshToken ? 60 * 60 * 7 : 60 * 60,
+    });
   }
 }
