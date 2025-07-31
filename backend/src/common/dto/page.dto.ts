c521a535ccaff5a2aae3066886071782e555270d
sesil <chhezue@gmail.com>
2025-07-31 22:58:58 +0900

    feat(product): 📚 pagination (2)

--- backend/src/common/dto/page.dto.ts
+++ backend/src/common/dto/page.dto.ts
@@ -1,12 +1,14 @@
 import { Type } from 'class-transformer';
-import { IsNumber, IsOptional } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class PageDto {
+  @ApiProperty({ description: '커서 값 (ISO Date String)' })
   @IsOptional()
-  @Type(() => Number)
-  @IsNumber()
-  readonly cursor?: number;
+  @IsString()
+  readonly cursor?: string;
 
+  @ApiProperty({ description: '페이지당 항목 수' })
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
