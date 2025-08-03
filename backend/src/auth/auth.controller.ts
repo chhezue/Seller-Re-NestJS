436221e748aabd20407204807003ccc0006d50eb
KimGyeJeong <pmwkdo@gmail.com>
2025-08-04 00:39:01 +0900

    feat: 🔐 Refactor authentication with Guards and Decorators

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,13 +1,30 @@
-import { Controller, Post, Headers, Req, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Headers,
+  Req,
+  Body,
+  UseGuards,
+  Get,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Request } from 'express';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { IsPublic } from '../common/decorator/is-public.decorator';
+import {
+  AccessTokenGuard,
+  RefreshTokenGuard,
+} from './guard/bearer-token.guard';
+import { User } from '../users/decorator/user.decorator';
+import { UsersModel } from '../users/entity/users.entity';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @IsPublic()
+  @UseGuards(RefreshTokenGuard)
   async postLoginEmail(
     @Headers('authorization') rawToken: string,
     @Req() req: Request,
@@ -20,16 +37,21 @@ export class AuthController {
   }
 
   @Post('users')
+  @IsPublic()
   async postRegister(@Body() registerUserDto: RegisterUserDto) {
     return await this.authService.registerWithEmail(registerUserDto);
   }
 
   @Post('token/access')
+  @IsPublic()
+  @UseGuards(RefreshTokenGuard)
   async postTokenAccess(@Headers('authorization') rawToken: string) {
     return await this.reissueToken(rawToken, false);
   }
 
   @Post('token/refresh')
+  @IsPublic()
+  @UseGuards(RefreshTokenGuard)
   async postTokenRefresh(@Headers('authorization') rawToken: string) {
     return await this.reissueToken(rawToken, true);
   }
@@ -41,5 +63,12 @@ export class AuthController {
     const tokenType = isRefresh ? 'refreshToken' : 'accessToken';
 
     return { [tokenType]: newToken };
+  }
+
+  @Get('test/here')
+  @UseGuards(AccessTokenGuard)
+  async testHere(@User() user: UsersModel) {
+    console.log('testHere.user : ', user);
+    return user;
   }
 }
