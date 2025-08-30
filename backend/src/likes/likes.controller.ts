c1e0b6f653ccf42224b352eefbd13cbc94931fde
sesil <chhezue@gmail.com>
2025-08-31 05:20:03 +0900

    feat(product): 상품 찜 기능

--- backend/src/likes/likes.controller.ts
+++ backend/src/likes/likes.controller.ts
@@ -1,7 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { LikesService } from './likes.service';
+import { ApiOperation } from '@nestjs/swagger';
+import { User } from '../users/decorator/user.decorator';
+import { PageDto } from '../common/dto/page.dto';
 
 @Controller('likes')
 export class LikesController {
   constructor(private readonly likesService: LikesService) {}
+
+  @ApiOperation({ description: '나의 찜 목록 조회' })
+  @Get('/my')
+  getMyLikes(@User('id') userId: string, @Body() pageDto: PageDto) {
+    return this.likesService.getMyLikes(userId, pageDto);
+  }
+
+  @ApiOperation({ description: '상품 찜하기' })
+  @Post('/:productId')
+  createLike(
+    @Param('productId') productId: string,
+    @User('id') userId: string,
+  ) {
+    return this.likesService.createLike(productId, userId);
+  }
+
+  @ApiOperation({ description: '상품 찜 취소' })
+  @Delete('/:productId')
+  deleteLike(
+    @Param('productId') productId: string,
+    @User('id') userId: string,
+  ) {
+    return this.likesService.deleteLike(productId, userId);
+  }
 }
--- backend/src/likes/likes.module.ts
+++ backend/src/likes/likes.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { LikesService } from './likes.service';
 import { LikesController } from './likes.controller';
 import { LikeModel } from './entity/like.entity';
+import { ProductModel } from '../product/entity/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LikeModel])],
+  imports: [TypeOrmModule.forFeature([LikeModel, ProductModel])],
   controllers: [LikesController],
   providers: [LikesService],
   exports: [LikesService],
